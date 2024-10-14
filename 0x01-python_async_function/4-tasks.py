8ef2ebde076af7958f0453db7251c42a0e977e26
Lucy Ouma <akinyilucy09@gmail.com>
2024-10-14 21:46:58 +0300

    Pycodestyle errors

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
-"""Excecute multiple coroutines at the same time with async"""
-from typing import List
+""" Tasks """
 import asyncio
+from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with the specified max_delay
-        and returns the list of all the delays (float values)."""
-    futures = [task_wait_random(max_delay) for _ in range(n)]
-    futures = asyncio.as_completed(futures)
-    delays = [await future for future in futures]
-    return delays
+    """ Async routine.
+        Args:
+            max_delay: integer argument.
+            n: integer argument.
+        Return:
+            List of all the delays random float.
+    """
+    delays: List[float] = []
+    orderList: List[float] = []
+
+    for i in range(n):
+        delays.append(task_wait_random(max_delay))
+
+    for o in asyncio.as_completed(delays):
+        orderList.append(await o)
+
+    return orderList
