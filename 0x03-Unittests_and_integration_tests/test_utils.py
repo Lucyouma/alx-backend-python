9d064e14f336f211f45476f17200a4526d72c841
Lucy Ouma <akinyilucy09@gmail.com>
2024-12-23 03:17:23 +0300

    Pycodestyle error-fixing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,17 +15,17 @@ from utils import (
 
 class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
-     @parameterized.expand([
+    @parameterized.expand([
          ({"a": 1}, ("a",), 1),
          ({"a": {"b": 2}}, ("a",), {"b": 2}),
          ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(
-           self,
-           nested_map: Dict,
-           path: Tuple[str],
-           expected: Union[Dict, int],
-           ) -> None:        
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
         """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
